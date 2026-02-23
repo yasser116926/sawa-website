7dd302e4d4a233d9bd7e6dac0ad1bd460f936f4d
yasser116926 <yasserbasher222@gmail.com>
2026-02-23 07:36:50 +0300

    Fix static files for production

--- config/settings.py
+++ config/settings.py
@@ -131,17 +131,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
-
 STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
-
-STORAGES = {
-    "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
-    },
-}
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 ALLOWED_HOSTS = [
     "sawapeace.org",
     "www.sawapeace.org",
     ".onrender.com",
 ]
+
